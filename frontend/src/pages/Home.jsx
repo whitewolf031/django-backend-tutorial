a94d3f33002c8b577d8af1fa08d74a5a4ef9b00b
whitewolf031 <w72075433@gmail.com>
2026-02-23 15:37:52 +0500

    finish the login and register

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react"
+import api from "../api"
+import { data } from "react-router-dom";
+
+function Home () {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        getNote();
+    }, [])
+    
+    const getNote = () => {
+        api.get("api/notes/")
+        .then((res) => res.data)
+        .then((data) => {
+            setNotes(data);
+            console.log(data);
+        })
+        .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Note deleted!");
+                else alert("Failed to delete note.");
+                getNotes();
+            })
+            .catch((error) => alert(error));
+    };
+
+    const createNote = (e) => {
+        e.preventDefault();
+        api
+            .post("/api/notes/", { content, title })
+            .then((res) => {
+                if (res.status === 201) alert("Note created!");
+                else alert("Failed to make note.");
+                getNotes();
+            })
+            .catch((err) => alert(err));
+    };
+
+    return <div>Home</div>
+}
+
+export default Home
